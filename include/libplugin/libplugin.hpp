19f0280b61dd54ae560be4ebd432976514f93322
adriensalon <adrien.salon@live.fr>
2023-08-25 01:02:42 +0200

    VST2 & VST3 perfect go AAX

--- include/libplugin/libplugin.hpp
+++ include/libplugin/libplugin.hpp
@@ -17,10 +17,10 @@
 #include <libplugin/libplugin.fwd>
 
 /// @brief
-#define REGISTER_PLUGIN(processor_class_m, controller_class_m) \
-	VERIFY_PLUGIN(processor_class_m, controller_class_m) \
-	ADD_PLUGIN(processor_class_m, controller_class_m)\
-	ADD_PLUGIN_VST2_WRAPPER(processor_class_m, controller_class_m) \
-	ADD_PLUGIN_AAX_WRAPPER(processor_class_m, controller_class_m) \
-	ADD_PLUGIN_AU2_WRAPPER(processor_class_m, controller_class_m) \
-	ADD_PLUGIN_AU3_WRAPPER(processor_class_m, controller_class_m) 
+#define REGISTER_PLUGIN(processor_class_m, controller_class_m)    \
+    VERIFY_PLUGIN(processor_class_m, controller_class_m)          \
+    ADD_PLUGIN(processor_class_m, controller_class_m)             \
+    ADD_PLUGIN_AAX_WRAPPER(processor_class_m, controller_class_m) \
+    ADD_PLUGIN_AU2_WRAPPER(processor_class_m, controller_class_m) \
+    ADD_PLUGIN_AU3_WRAPPER(processor_class_m, controller_class_m) \
+    ADD_PLUGIN_VST2_WRAPPER(processor_class_m, controller_class_m)
